819e6a16370318eea2aa4a579f37910ef011d440
romgarofficial <rnvelasco@yahoo.com>
2025-05-14 10:52:57 +0800

    added state management for sign up

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,8 +4,22 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import {useState} from 'react';
 
 export default function SignUp(){
+
+    const [firstName, setFirstName] = useState("");
+    const [middleName, setMiddleName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [dob, setDob] = useState(null);
+    const [email, setEmail] = useState("");
+    const [mobile, setMobile] = useState("");
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
+
+    console.log(firstName);
+    console.log(dob)
+
     return(
         <Container fluid className="">
             <Row>
@@ -18,22 +32,71 @@ export default function SignUp(){
 
                         <Form>
                             <Container fluid className="d-flex flex-column gap-3 mt-5">
-                                <TextField id="outlined-basic" label="First Name" variant="outlined" />
-                                <TextField id="outlined-basic" label="Middle Name" variant="outlined" />
-                                <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+
+                                <TextField id="outlined-basic" label="First Name" 
+                                variant="outlined" 
+                                onChange={e => setFirstName(e.target.value)}
+                                value={firstName}
+                                />
+
+                                <TextField id="outlined-basic" label="Middle Name" variant="outlined" 
+                                onChange={e => setMiddleName(e.target.value)}
+                                value={middleName}
+                                />
+
+                                <TextField id="outlined-basic" label="Last Name" variant="outlined" 
+                                onChange={e => setLastName(e.target.value)}
+                                value={lastName}
+                                />
 
                                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                                 <DemoContainer components={['DatePicker']}>
-                                    <DatePicker label="Basic date picker" className="w-100"/>
+                                    <DatePicker label="Date of Birth" className="w-100"
+                                        onChange={e => setDob(e)}
+                                        value={dob}
+                                    />
                                 </DemoContainer>
                                 </LocalizationProvider>
                                 
-                                <TextField id="outlined-basic" label="Email" variant="outlined" />
-                                <TextField id="outlined-basic" label="Mobile Number" variant="outlined" />
-                                <TextField id="outlined-basic" label="Password" variant="outlined" />
-                                <TextField id="outlined-basic" label="Confirm Password" variant="outlined" />
+                                <TextField id="outlined-basic" label="Email" variant="outlined" 
+                                    onChange={e => setEmail(e.target.value)}
+                                    value={email}
+                                />
+
+                                <TextField id="outlined-basic" label="Mobile Number" variant="outlined" 
+                                    onChange={e => setMobile(e.target.value)}
+                                    value={mobile}
+                                />
+
+                                <TextField id="outlined-basic" label="Password" variant="outlined" 
+                                    onChange={e => setPassword1(e.target.value)}
+                                    value={password1}
+                                    type="password"
+                                />
+                                
+                                <TextField id="outlined-basic" label="Confirm Password" variant="outlined" 
+                                    onChange={e => setPassword2(e.target.value)}
+                                    value={password2}
+                                    type="password"
+                                />
+
+                                {
+                                    password1 === ""
+                                    ?
+                                    <p className="text-success d-none">
+                                    </p>
+                                    :
+                                    password1 !== password2
+                                    ?
+                                    <p className="text-danger">
+                                    Password do not match.</p>
+                                    :
+                                    <p className="text-success">
+                                    Password confirmed!</p>
+                                }
 
                                 <Button variant="contained" className="p-3 rounded-3 mt-4">Sign Up</Button>
+
                             </Container>
                         </Form>
 
